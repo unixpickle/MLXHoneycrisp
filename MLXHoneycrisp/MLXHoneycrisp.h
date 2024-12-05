65dd48e89106bf3296cdd7c903b05c2a0310d3da
Alex Nichol <unixpickle@gmail.com>
2024-12-04 19:06:56 -0500

    Initial Commit

---
+++ MLXHoneycrisp/MLXHoneycrisp.h
@@ -0,0 +1,18 @@
+//
+//  MLXHoneycrisp.h
+//  MLXHoneycrisp
+//
+//  Created by Alex Nichol on 12/4/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MLXHoneycrisp.
+FOUNDATION_EXPORT double MLXHoneycrispVersionNumber;
+
+//! Project version string for MLXHoneycrisp.
+FOUNDATION_EXPORT const unsigned char MLXHoneycrispVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MLXHoneycrisp/PublicHeader.h>
+
+
